cc2d04051f307a0fae701cce528eed4586b35504
Kamilly Mattana <kamilly.mattana@pucpr.edu.br>
2026-05-21 21:10:28 -0300

    Configuração das páginas Cadastro e Login (falta adicionar foto e outro input de senha no cadastro)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import logoImg from "../assets/logo.png";
 import "../styles/login.css";
+import "../styles/loginCadastro.css";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
@@ -42,48 +43,61 @@ function Login() {
   return (
     <div>
       <Navbar />
-      <div className="caixaLogin">
-        <div className="login-logo-wrapper">
-          <img src={logoImg} alt="Nouvelle"/>
+
+      <div id="divLogin">
+
+        <div id="divLeft">
+          <div className="Tamanho_Logo">
+            <img src={logoImg} alt="Nouvelle"/>
+    
+            {mensagem && (
+              <p style={{ color: "red", marginTop: "15px", textAlign: "center" }}>
+                {mensagem}
+              </p>
+            )}
+          </div>
         </div>
-
-        <form onSubmit={handleSubmit} id="formLogin">
-          <div className="login_senha_caixas">
-            <input
-              type="email"
-              placeholder="E-mail"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="login_senha_caixas">
-            <input
-              type="password"
-              placeholder="Senha"
-              value={senha}
-              onChange={e => setSenha(e.target.value)}
-              required
-            />
-          </div>
-
-          <button type="submit" className="btn-login">Entrar</button>
-
-          {mensagem && (
-            <p style={{ color: "red", marginTop: "15px", textAlign: "center" }}>
-              {mensagem}
-            </p>
-          )}
-
-          <div className="utilidades">
-            <div className="lembre_de_mim">
-              <input type="checkbox" id="remember" />
-              <label htmlFor="remember">Lembre-se de mim</label>
+        
+        <div id="divRight">
+          <h1 className="tituloPagina">PÁGINA DE CADASTRO</h1>
+          
+          <form onSubmit={handleSubmit} id="formLogin">
+            <div className="login_senha_caixas">
+              <input
+                type="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
             </div>
-            <a href="#" className="Qual_minha_senha">Esqueceu sua senha?</a>
-          </div>
-        </form>
+            <div className="login_senha_caixas">
+              <input
+                type="password"
+                placeholder="Senha"
+                value={senha}
+                onChange={e => setSenha(e.target.value)}
+                required
+              />
+            </div>
+
+            <button type="submit" className="btn-login">Entrar</button>
+
+            
+
+            <div className="utilidades">
+              <div className="lembre_de_mim">
+                <input type="checkbox" id="remember" />
+                <label htmlFor="remember">Lembre-se de mim</label>
+              </div>
+              <a href="#" className="Qual_minha_senha">Esqueceu sua senha?</a>
+            </div>
+          </form>
+        </div>
+        
       </div>
+
+
     </div>
   );
 }
